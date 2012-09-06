dd8b9e11864b64a263b3af88b6109e5a4cb3e312
devand123 <devand123@gmail.com>
2012-09-06 00:09:45 -0400

    this is a test commit

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,19 @@
 require 'erb'
+require 'date'
+require 'time'
 
-x = 42
-template = ERB.new "The value of x is: <%= x %>"
+x = 42 + 55
+
+pets = ['dogs', 'cats', 'lizards']
+
+pets.each do |pet|
+	puts "I love #{pet}"
+end
+
+template = ERB.new "The value of x is: <%= x %>. "
+template2 = ERB.new "There are <%= pets.count %> pets."
+template3 = ERB.new "Todays date is <%= Date.today %> and it is <%= Time.now %>"
+
 puts template.result(binding)
+puts template2.result(binding)
+puts template3.result(binding)
