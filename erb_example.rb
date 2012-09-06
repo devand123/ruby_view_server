c42c7fc6c017236eff7bb5c6c1b81c4fc661af6b
devand123 <devand123@gmail.com>
2012-09-06 00:12:53 -0400

    this is a commit test 2

--- erb_example.rb
+++ erb_example.rb
@@ -12,7 +12,7 @@ end
 
 template = ERB.new "The value of x is: <%= x %>. "
 template2 = ERB.new "There are <%= pets.count %> pets."
-template3 = ERB.new "Todays date is <%= Date.today %> and it is <%= Time.now %>"
+template3 = ERB.new "It is <%= Time.now %>. Tomorrow is <%= Time.tomorrow %>"
 
 puts template.result(binding)
 puts template2.result(binding)
